12092bcb6e53cff95bd4edefd002d55d56867224
josahn21 <42394066+josahn21@users.noreply.github.com>
2019-04-11 12:55:54 -1000

    Delete platform.js

--- platform.js
+++
@@ -1,9 +0,0 @@
-function platform() {
-  this.x = Math.floor(Math.random()*w);
-  this.y = Math.floor(Math.random()*h);
-  
-  this.show = function() {
-    fill(color("orange"));
-    rect(this.x, this.y, 50,50);
-  }
-}
